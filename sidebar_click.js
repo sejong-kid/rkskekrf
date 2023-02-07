6f6bcc78efb594ce2c3afe27cdaf3d21d7385f24
sejong-kid <123qocksgh@naver.com>
2023-02-07 17:46:47 +0900

    ss

--- sidebar_click.js
+++ sidebar_click.js
@@ -8,3 +8,24 @@ for (let i=0; i<list.length; i++){
     list[i].className = 'list active';
   }
 }
+
+var bDisplay = true;
+function Recruitment(){
+  var con = document.getElementById("category1");
+  var von = document.getElementById("door")
+  if(con.style.display=='none'){
+    con.style.display = 'block';
+    von.style.display='none';
+  }else{
+    con.style.display='none';
+    von.style.display='block';
+  }
+}
+function door(){
+  var con = document.getElementById("category1");
+  var von = document.getElementById("door")
+  if(von.style.display=='none'){
+    von.style.display='block';
+    con.style.display='none';
+  }
+}
