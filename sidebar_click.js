9e97b247d63e0acbf1626598aa45226912a95dd7
sejong-kid <123qocksgh@naver.com>
2023-02-08 17:11:27 +0900

    23.02.08

--- sidebar_click.js
+++ sidebar_click.js
@@ -1,3 +1,6 @@
+// 사이드바 각 메뉴 선택시 하얀 배경 유지하는 스크립트
+// 이걸로 각 메뉴 선택 이후 나오는 div의 상단 버튼들도 색상 변경을 해야하는데
+// 이 스크립트의 내용을 이해하지 못해서 지정하지 못함.
 let list = document.querySelectorAll('.list');
 for (let i=0; i<list.length; i++){
   list[i].onclick = function(){
@@ -9,23 +12,94 @@ for (let i=0; i<list.length; i++){
   }
 }
 
+// 아래의 스크립트를 간결하게 할 수 있을 것 같은데....
+// 채용 및 모집 클릭시 다른 div 숨기기
 var bDisplay = true;
-function Recruitment(){
+function Recruitment1(){
   var con = document.getElementById("category1");
   var von = document.getElementById("door")
+  var btn1 = document.getElementById("btn1")
+  var btn2 = document.getElementById("btn2")
+  var btn3 = document.getElementById("btn3")
+  var text = document.getElementById("serchtext")
   if(con.style.display=='none'){
     con.style.display = 'block';
     von.style.display='none';
+    btn1.style.display = 'block';
+    btn2.style.display = 'none';
+    btn3.style.display = 'none';
+    text.style.display = 'none';
   }else{
-    con.style.display='none';
-    von.style.display='block';
+    con.style.display='block';
+    von.style.display='none';
+    btn1.style.display = 'block';
+    btn2.style.display = 'none';
+    btn3.style.display = 'none';
+    text.style.display = 'none';
   }
 }
+
+// 참여 클릭시 다른 div 숨기기
+
+function Recruitment2(){
+  var con = document.getElementById("category1");
+  var von = document.getElementById("door")
+  var btn1 = document.getElementById("btn1")
+  var btn2 = document.getElementById("btn2")
+  var btn3 = document.getElementById("btn3")
+  var text = document.getElementById("serchtext")
+  if(con.style.display=='none'){
+    con.style.display = 'block';
+    von.style.display='none';
+    btn1.style.display = 'none';
+    btn2.style.display = 'block';
+    btn3.style.display = 'none';
+    text.style.display = 'none';
+  }else{
+    con.style.display='block';
+    von.style.display='none';
+    btn1.style.display = 'none';
+    btn2.style.display = 'block';
+    btn3.style.display = 'none';
+    text.style.display = 'none';
+  }
+}
+
+// 기타 클릭시 다른 div 숨기기
+
+function Recruitment3(){
+  var con = document.getElementById("category1");
+  var von = document.getElementById("door")
+  var btn1 = document.getElementById("btn1")
+  var btn2 = document.getElementById("btn2")
+  var btn3 = document.getElementById("btn3")
+  var text = document.getElementById("serchtext")
+  if(con.style.display=='none'){
+    con.style.display = 'block';
+    von.style.display='none';
+    btn1.style.display = 'none';
+    btn2.style.display = 'none';
+    btn3.style.display = 'block';
+    text.style.display = 'none';
+  }else{
+    con.style.display='block';
+    von.style.display='none';
+    btn1.style.display = 'none';
+    btn2.style.display = 'none';
+    btn3.style.display = 'block';
+    text.style.display = 'none';
+  }
+}
+
+// 충녕봇 클릭시 처음 화면으로
+
 function door(){
   var con = document.getElementById("category1");
   var von = document.getElementById("door")
+  var text = document.getElementById("serchtext")
   if(von.style.display=='none'){
     von.style.display='block';
     con.style.display='none';
+    text.style.display='none';
   }
 }
--- text_area.js
+++ text_area.js
@@ -1,3 +1,34 @@
-setInterval(function(){
-  $(".blinkEle").toggle();
-}, 250);
+// 검색창 관련 스크립트
+// 검색창 내용 입력 후 검색을 누르면 나오는 div 스크립트
+// 이 부분은 손을 봐야함
+
+
+// 버그 리포트
+// 1. 내용 입력후 돋보기 모양 클릭시 충녕봇 초기 화면으로 넘어감
+// 2. 사이드바 메뉴 선택 후 돋보기 모양을 클릭하면 해당 div로 넘어가지만
+// 3. 사이드바 메뉴 선택 된 것(하얀 배경)이 초기화가 안됨
+
+var bDisplay = true;
+function serchbtn(){
+  var con = document.getElementById("category1");
+  var von = document.getElementById("door")
+  var btn1 = document.getElementById("btn1")
+  var btn2 = document.getElementById("btn2")
+  var btn3 = document.getElementById("btn3")
+  var text = document.getElementById("serchtext")
+  if(con.style.display=='none'){
+    con.style.display = 'block';
+    von.style.display='none';
+    btn1.style.display = 'none';
+    btn2.style.display = 'none';
+    btn3.style.display = 'none';
+    text.style.display = 'block';
+  }else{
+    con.style.display='block';
+    von.style.display='none';
+    btn1.style.display = 'none';
+    btn2.style.display = 'none';
+    btn3.style.display = 'none';
+    text.style.display = 'block';
+  }
+}
